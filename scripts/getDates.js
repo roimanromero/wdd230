babc66ccfcd67a6981e636d2a007dedc2dde7985
Roiman Romero <roimanromerorobinson@gmail.com>
2024-06-27 13:22:49 -0500

    website changes

---
+++ scripts/getDates.js
@@ -0,0 +1,3 @@
+// getDates.js
+document.getElementById("footer-info").textContent = `© ${new Date().getFullYear()} Roiman Romero, Colombia`;
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;

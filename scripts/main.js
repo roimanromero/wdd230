77f86a8f80540f7423e97e251de84bdc81f19d72
Roiman Romero <roimanromerorobinson@gmail.com>
2024-07-16 21:35:30 -0500

    added form

--- scripts/main.js
+++ scripts/main.js
@@ -32,3 +32,11 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem("visits", visits);
     visitsDisplay.textContent = visits;
 });
+document.getElementById('hamburger').addEventListener('click', function() {
+    var navMenu = document.getElementById('nav-menu');
+    if (navMenu.classList.contains('hidden')) {
+        navMenu.classList.remove('hidden');
+    } else {
+        navMenu.classList.add('hidden');
+    }
+});
